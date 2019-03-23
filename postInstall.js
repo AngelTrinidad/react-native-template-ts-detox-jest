46feab54532ba32a432b8ed53bce04fb05698669
Jeff Held <jheld135@gmail.com>
2019-03-23 15:48:39 -0500

    Move to one package.json

--- postInstall.js
+++ postInstall.js
@@ -17,16 +17,36 @@ const logAndDelete = (file) =>
   logAndDo(format("Deleting %s", file), () => deleteFile(file));
 
 const packageJson = require("./package.json");
+const appName = require("./app.json").name;
 
 console.log("Setting up project");
 
 logAndDo("Modifying package.json ", () => {
   packageJson.scripts.tsc = "tsc";
+  packageJson.scripts.lint = undefined;
   // Remove jest configuration as it will be defined in jest.config.js
   packageJson.jest = undefined;
 
   // Remove babel-jest from devDependencies since ts-jest will be used instead
   packageJson.devDependencies["babel-jest"] = undefined;
+  packageJson.devDependencies.eslint = undefined;
+  packageJson.devDependencies[
+    "@react-native-community/eslint-config"
+  ] = undefined;
+
+  const detox = require("./detox.config.json");
+  Object.keys(detox.configurations)
+    .filter((x) => x.startsWith("ios."))
+    .forEach((config) => {
+      detox.configurations[config].build = detox.configurations[
+        config
+      ].build.replace(/__APP_NAME__/g, appName);
+      detox.configurations[config].binaryPath = detox.configurations[
+        config
+      ].binaryPath.replace(/__APP_NAME__/g, appName);
+    });
+
+  packageJson.detox = detox;
 
   writeJsonToFile("package.json", packageJson);
 });
@@ -34,6 +54,7 @@ logAndDo("Modifying package.json ", () => {
 logAndDelete(".flowconfig");
 logAndDelete("App.js");
 logAndDelete("postInstall.js");
+logAndDelete("detox.config.json");
 
 console.log("Setup completed!");
 console.log(`
